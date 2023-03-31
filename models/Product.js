909345ded81117dfb5b7bba4cf6e5b7c20734c05
daveS <davesimpcontrol@gmail.com>
2023-03-30 19:29:36 -0600

    added validators, all known acceptance criteria met

--- models/Product.js
+++ models/Product.js
@@ -22,11 +22,18 @@ Product.init(
   },
   price: {
     type: DataTypes.DECIMAL,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
   },
   stock: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
   },
   category_id: {
     type: DataTypes.INTEGER,
@@ -34,7 +41,8 @@ Product.init(
       model: 'category',
       key: 'id',
       unique: false
-    }
+    },
+    
   }
 },
   {
